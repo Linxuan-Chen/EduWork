4f7aebbce69b1d640461822c0ecf8589f3a71e45
Linxuan-Chen <moshouclxclx@163.com>
2020-08-06 18:50:21 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Linxuan-Chen.github.io/Linxuan-Chen/precache-manifest.bc3796bdd4a98480f286d4bca5aa37f0.js"
+  "//My-Webpage/precache-manifest.475a24c91860f1d38d766e17a4df673a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Linxuan-Chen.github.io/Linxuan-Chen/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//My-Webpage/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
